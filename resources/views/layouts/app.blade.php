68ceb01e6290b84ee4cbef627dbc1d01216d7db3
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-05-09 10:34:27 +0500

    design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,19 +18,18 @@
 
  </head>
 <body id="app-layout">
-@include('partials.navigation')
-
-<div class="container">
-
         {{--Navigation--}}
+        @include('partials.navigation')
 
+        <div class="container">
+            @yield('content')
+        </div>
 
-    @yield('content')
-</div>
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="/js/main.js"></script>
+    <!--flash messages-->
     @include('messages.flash')
 </body>
 </html>
