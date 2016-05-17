07b2e8983749ee8999ef05734f5c9cd5e6ae1c46
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-05-17 17:04:43 +0500

    beanstalkd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="/js/main.js"></script>
-      @include('messages.flash')
-      {{--Скрипты--}}
+
+    @include('messages.flash')
     @yield('scripts.footer')
 </body>
 </html>
