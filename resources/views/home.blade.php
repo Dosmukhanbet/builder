1bca5c07dfddcb90f64cfe9df8ace3a2dca2ce70
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-06-20 20:35:20 +0500

    home page styling for media

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,15 +21,15 @@
       <div class="row">
       <div class="col-md-10 col-md-offset-1">
         <div class="col-md-4 steps">
-          <h4><span>1</span> Heading</h4>
+          <h4><span>1</span> Создайте заявку</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         </div>
         <div class="col-md-4 steps">
-          <h4><span>2</span> Heading</h4>
+          <h4><span>2</span> Найдите мастера</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
        </div>
         <div class="col-md-4 steps">
-          <h4><span>3</span> Heading</h4>
+          <h4><span>3</span> Оставьте отзыв</h4>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         </div>
         </div>
