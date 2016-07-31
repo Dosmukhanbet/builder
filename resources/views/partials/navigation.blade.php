94e61951a8deb42cc4b6db18f039b70ba16f9227
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-07-31 21:56:27 +0500

    navigtion style changed

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -30,8 +30,8 @@
                                             <ul class="nav navbar-nav navbar-right Nav__list">
                                                  <li><a class="Nav__links" href="{{ url('/create/registerandcreatejob') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Создать заявку</a></li>
                                                  <li><a class="Nav__links" href="{{url('find/masters')}}"><i class="fa fa-search" aria-hidden="true"></i>Найти мастера</a></li>
-                                                 <li class="register"><a class="Nav__links" href="{{ url('/register') }}">Регистрация</a></li>
                                                  <li class="login"><a class="Nav__links" href="{{ url('/login') }}">Вход</a></li>
+                                                 <li class="register"><a class="Nav__links" href="{{ url('/register') }}">Регистрация</a></li>
                                             </ul>
 
                                     @endif
