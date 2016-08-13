c51015ec0e33d365659fc6ef102c9750093d699d
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-08-13 20:34:32 +0500

    home page design

--- resources/views/homepartials/howto.blade.php
+++ resources/views/homepartials/howto.blade.php
@@ -1,22 +1,24 @@
 <div class="container">
       <div class="row">
             <div class="col-md-6 how_to">
-                    <h3 class="how_to_heading">Ищите мастера ?</h3>
-                    <div class="col-md-10 col-md-offset-2 steps">
+                    <div class="col-md-9 col-md-offset-3">
+                      <h3 class="how_to_heading">Ищите мастера ?</h3>
+                    </div>
+                    <div class="col-md-9 col-md-offset-3 steps">
                       <h4><span>1</span> Создайте заявку</h4>
                       <p>
                       Это просто и всегда бесплатно! <br>
                       Как только Ваша заявка будет в нашем Клубе, ею заинтересуются и будут готовы выполнить высококлассные мастера
                       </p>
                     </div>
-                    <div class="col-md-10 col-md-offset-2 steps">
+                    <div class="col-md-9 col-md-offset-3 steps">
                       <h4><span>2</span> Найдите мастера</h4>
                       <p>
                        Выберите мастера из Вашего региона любого дела.<br>
                        Свяжитесь с ними напрямую
                        </p>
                     </div>
-                    <div class="col-md-10 col-md-offset-2 steps">
+                    <div class="col-md-9 col-md-offset-3 steps">
                       <h4><span>3</span> Оставьте отзыв</h4>
                       <p>
                         Оставьте отзыв о проделанной работе, оцените работу мастера и порекомендуйте его друзьям
@@ -24,22 +26,24 @@
                     </div>
             </div>
             <div class="col-md-6 formasters">
-                                      <h3 class="how_to_heading"> Вы мастер ?</h3>
-                                      <div class="col-md-10 col-md-offset-1 steps">
+                                      <div class="col-md-9">
+                                        <h3 class="how_to_heading_formasters"> Вы мастер ?</h3>
+                                      </div>
+                                      <div class="col-md-9 steps">
                                         <h4><span>1</span> Зарегистрируйтесь</h4>
                                         <p>
                                         Зарегистрируйтесь как мастер, Вам будут доступны все заявки Вашего города по Вашей специальности
 
                                         </p>
                                       </div>
-                                      <div class="col-md-10 col-md-offset-1 steps">
+                                      <div class="col-md-9 steps">
                                         <h4><span>2</span> Отправьте предложение</h4>
                                         <p>
                                          Отправьте предложение клиенту, работу которого вы готовы выполнить  <br>
 
                                          </p>
                                       </div>
-                                      <div class="col-md-10 col-md-offset-1  steps">
+                                      <div class="col-md-9 steps">
                                         <h4><span>3</span> Свяжитесь с клиентом</h4>
                                         <p>
                                           Свяжитесь с клиентом принявшего Ваше предложение, обговорите детали работы.
