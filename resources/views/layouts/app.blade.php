5b688a0bcb78ef490f9cde84c251ea888f7103b1
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-08-15 20:03:04 +0500

    Yandex metrikz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,9 @@
     {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">--}}
      <link href="/css/app.css" rel="stylesheet">
      <link href="/css/all.css" rel="stylesheet">
+     @include('services.yandexmetrika')
 </head>
+
 <body id="app-layout">
         {{--Navigation--}}
             @include('partials.navigation')
