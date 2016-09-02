91957e22b77ded317df111d448788a9a1f13a426
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-09-02 19:53:57 +0500

    new

--- public/Панель управления JivoSite_files/browserNameVer.js
+++
@@ -1,9 +0,0 @@
-function browserNameVer() {
-    var N = navigator.appName, ua = navigator.userAgent.toLowerCase(), p = navigator.platform.toLowerCase(), tem;
-    var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-    var mac = p ? /mac/.test(p) : /mac/.test(ua);
-    if(M && (tem = ua.match(/version\/([\.\d]+)/i))!= null) M[2] = tem[1];
-    M = M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
-    M.mac = mac;
-    return M;
-}
