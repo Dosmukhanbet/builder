71cf339f467e8aa13e0f905546ad5a2012ca2701
Dosmukhanbet <dosmukhanbet@gmail.com>
2016-09-17 18:11:35 +0500

    e

--- resources/views/partials/adminnav.blade.php
+++ resources/views/partials/adminnav.blade.php
@@ -1,4 +1,6 @@
 <ul class="nav navbar-nav navbar-right Nav__list">
                                        <li><a class="Nav__links" href="{{ url('admin/users') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Users</a></li>
                                         <li><a class="Nav__links" href="{{url('/')}}"><i class="fa fa-search" aria-hidden="true"></i>Settings</a></li>
+                                       <li><a class="Nav__links" href="{{ url('/logout') }}"><i class="fa fa-share-square-o" aria-hidden="true"></i>Выйти</a></li>
+										
 </ul>
